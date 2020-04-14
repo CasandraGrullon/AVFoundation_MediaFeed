f48bfe57c46a268362f3a850aeccb3ac138cc16e
Casandra <casandragrullon@pursuit.org>
2020-04-14 12:16:18 -0400

    added core data stack and created a core data model

---
+++ AVFoundation_MediaFeed/CoreData/CoreDataManager.swift
@@ -0,0 +1,9 @@
+//
+//  CoreDataManager.swift
+//  AVFoundation_MediaFeed
+//
+//  Created by casandra grullon on 4/14/20.
+//  Copyright © 2020 casandra grullon. All rights reserved.
+//
+
+import Foundation
